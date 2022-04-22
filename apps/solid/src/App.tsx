2dce095943220281222c0c52ec79495e37183333
anymaniax <bury.victor@gmail.com>
2022-04-22 10:38:45 +0200

    fix(prettier): format

--- apps/solid/src/App.tsx
+++ apps/solid/src/App.tsx
@@ -1,5 +1,5 @@
-import type {Component} from 'solid-js';
-import {button} from 'styles/components/button.css';
+import type { Component } from 'solid-js';
+import { button } from 'styles/components/button.css';
 import styles from './App.module.css';
 import logo from './logo.svg';
 
@@ -11,7 +11,7 @@ const App: Component = () => {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <button class={button({color: 'neutral'})}>test</button>
+        <button class={button({ color: 'neutral' })}>test</button>
         <a
           class={styles.link}
           href="https://github.com/solidjs/solid"
--- apps/solid/vite.config.ts
+++ apps/solid/vite.config.ts
@@ -1,11 +1,11 @@
-import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
-import { defineConfig } from "vite";
-import solidPlugin from "vite-plugin-solid";
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
+import { defineConfig } from 'vite';
+import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
   plugins: [solidPlugin(), vanillaExtractPlugin()],
   build: {
-    target: "esnext",
+    target: 'esnext',
     polyfillDynamicImport: false,
   },
 });
