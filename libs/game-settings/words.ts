9ba2d4ad078bdf1d1194b1a882aa89ba1cf55a6b
messenjer <mathieu@mirahi.io>
2022-04-22 11:16:53 +0200

    refactor(libs): format

--- libs/game-settings/words.ts
+++ libs/game-settings/words.ts
@@ -1,27 +1,27 @@
-const defaultMessage = ' Using word of the day instead.'
+const defaultMessage = ' Using word of the day instead.';
 
 export function getWordOfTheDay() {
   if (location.search) {
     try {
-      const query = atob(location.search.slice(1))
+      const query = atob(location.search.slice(1));
       if (query.length !== 5) {
-        alert(`Incorrect word length from encoded query. ${defaultMessage}`)
+        alert(`Incorrect word length from encoded query. ${defaultMessage}`);
       } else {
-        return query
+        return query;
       }
     } catch (e) {
-      alert(`Malformed encoded word query. ${defaultMessage}`)
+      alert(`Malformed encoded word query. ${defaultMessage}`);
     }
   }
 
-  const now = new Date()
-  const start = new Date(2022, 0, 0)
-  const diff = Number(now) - Number(start)
-  let day = Math.floor(diff / (1000 * 60 * 60 * 24))
+  const now = new Date();
+  const start = new Date(2022, 0, 0);
+  const diff = Number(now) - Number(start);
+  let day = Math.floor(diff / (1000 * 60 * 60 * 24));
   while (day > answers.length) {
-    day -= answers.length
+    day -= answers.length;
   }
-  return answers[day]
+  return answers[day];
 }
 
 // copied from Wordle source
@@ -2340,8 +2340,8 @@ const answers = [
   'rower',
   'artsy',
   'rural',
-  'shave'
-]
+  'shave',
+];
 
 const allowedGuesses = [
   'aahed',
@@ -13000,7 +13000,7 @@ const allowedGuesses = [
   'zygal',
   'zygon',
   'zymes',
-  'zymic'
-]
+  'zymic',
+];
 
-export const allWords = [...answers, ...allowedGuesses]
+export const allWords = [...answers, ...allowedGuesses];
