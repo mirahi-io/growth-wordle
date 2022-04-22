71a37563a0d0575dc2f46432535f5509c5c9a271
messenjer <mathieu@mirahi.io>
2022-04-22 11:46:22 +0200

    feat(board): add board component

--- apps/solid/src/App.tsx
+++ apps/solid/src/App.tsx
@@ -2,7 +2,8 @@ import { Component, createEffect, createSignal } from 'solid-js';
 import { button } from 'styles/components/button.css';
 import styles from './App.module.css';
 import { Keyboard } from './components/Keyboard';
-import logo from './logo.svg';
+import { Board } from './components/Board';
+
 import { getWordOfTheDay } from 'game-settings';
 
 const App: Component = () => {
@@ -13,22 +14,8 @@ const App: Component = () => {
 
   return (
     <div class={styles.App}>
-      {getWordOfTheDay()}
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <button class={button({ color: 'neutral' })}>test</button>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
+      <button class={button({ color: 'neutral' })}>test</button>
+      <Board wordOfTheDay={getWordOfTheDay()} />
       <Keyboard setActiveKey={setActiveKey} />
     </div>
   );
