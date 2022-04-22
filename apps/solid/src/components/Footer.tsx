37ba64f7569ada73fe1ca2073e2d065c354409e8
messenjer <mathieu@mirahi.io>
2022-04-22 16:39:21 +0200

    feat: add header and footer

---
+++ apps/solid/src/components/Footer.tsx
@@ -0,0 +1,19 @@
+export const Footer = () => {
+  return (
+    <footer style={{ 'padding-top': '1rem' }}>
+      <p>Powered by Mirahi</p>
+      <p>
+        <a href="https://github.com/mirahi-io/wordle" target="_blank">
+          Show me the source
+        </a>
+      </p>
+      <img
+        width="50"
+        title="SolidJS Wordle by Mirahi"
+        alt="Logo Mirahi"
+        src="https://images.prismic.io/mirahi-io/33d20c07-9b58-48cb-af8e-152105009935_MIRAHI_FINAL-Logo_Plan+de+travail+1.svg?auto=format&amp;ixlib=react-9.0.2&amp;w=50"
+        srcset="https://images.prismic.io/mirahi-io/33d20c07-9b58-48cb-af8e-152105009935_MIRAHI_FINAL-Logo_Plan+de+travail+1.svg?auto=format&amp;ixlib=react-9.0.2&amp;w=50&amp;q=75&amp;dpr=1 1x, https://images.prismic.io/mirahi-io/33d20c07-9b58-48cb-af8e-152105009935_MIRAHI_FINAL-Logo_Plan+de+travail+1.svg?auto=format&amp;ixlib=react-9.0.2&amp;w=50&amp;q=50&amp;dpr=2 2x, https://images.prismic.io/mirahi-io/33d20c07-9b58-48cb-af8e-152105009935_MIRAHI_FINAL-Logo_Plan+de+travail+1.svg?auto=format&amp;ixlib=react-9.0.2&amp;w=50&amp;q=35&amp;dpr=3 3x, https://images.prismic.io/mirahi-io/33d20c07-9b58-48cb-af8e-152105009935_MIRAHI_FINAL-Logo_Plan+de+travail+1.svg?auto=format&amp;ixlib=react-9.0.2&amp;w=50&amp;q=23&amp;dpr=4 4x, https://images.prismic.io/mirahi-io/33d20c07-9b58-48cb-af8e-152105009935_MIRAHI_FINAL-Logo_Plan+de+travail+1.svg?auto=format&amp;ixlib=react-9.0.2&amp;w=50&amp;q=20&amp;dpr=5 5x"
+      ></img>
+    </footer>
+  );
+};
