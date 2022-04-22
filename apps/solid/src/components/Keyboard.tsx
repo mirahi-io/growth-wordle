ca3b88a191586f562961e84489567a8ac80fd4a0
Waryen <jonathangomand@gmail.com>
2022-04-22 13:16:52 +0200

    feat(easteregg): add easteregg

--- apps/solid/src/components/Keyboard.tsx
+++ apps/solid/src/components/Keyboard.tsx
@@ -1,5 +1,5 @@
 import { key } from 'styles/components/key.css';
-import { Setter, onMount, onCleanup } from 'solid-js';
+import { Setter, onMount, onCleanup, createSignal } from 'solid-js';
 
 const rows = [
   'qwertyuiop'.split(''),
@@ -15,6 +15,7 @@ export const Keyboard = ({ setActiveKey }: KeyboardProps) => {
   onMount(() => {
     document.addEventListener('keydown', keyboardListener);
     document.addEventListener('keyup', cleanKey);
+    easterEgg();
   });
   onCleanup(() => {
     document.removeEventListener('keydown', keyboardListener);
@@ -45,11 +46,51 @@ export const Keyboard = ({ setActiveKey }: KeyboardProps) => {
                 setActiveKey('');
               }}
             >
-              {keyValue}
+              {keyValue === 'Backspace' ? (
+                <svg
+                  v-else
+                  xmlns="http://www.w3.org/2000/svg"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  width="24"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M22 3H7c-.69 0-1.23.35-1.59.88L0 12l5.41 8.11c.36.53.9.89 1.59.89h15c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H7.07L2.4 12l4.66-7H22v14zm-11.59-2L14 13.41 17.59 17 19 15.59 15.41 12 19 8.41 17.59 7 14 10.59 10.41 7 9 8.41 12.59 12 9 15.59z"
+                  ></path>
+                </svg>
+              ) : (
+                keyValue
+              )}
             </button>
           ))}
         </div>
       ))}
     </div>
   );
+};
+
+const easterEgg = () => {
+  const pattern = ['m', 'i', 'r', 'a', 'i'];
+  const [current, setCurrent] = createSignal(0);
+
+  onMount(() => document.addEventListener('keydown', keyHandler));
+  onCleanup(() => document.removeEventListener('keydown', keyHandler));
+
+  const keyHandler = (e: KeyboardEvent) => {
+    // If the key isn't in the pattern, or isn't the current key in the pattern, reset
+    if (pattern.indexOf(e.key) < 0 || e.key !== pattern[current()]) {
+      setCurrent(0);
+      return;
+    }
+
+    // Update how much of the pattern is complete
+    setCurrent((prev) => prev + 1);
+
+    // If complete, alert and reset
+    if (pattern.length === current()) {
+      setCurrent(0);
+      console.log('mirai');
+    }
+  };
 };
