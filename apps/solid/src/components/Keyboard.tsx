8bac095da514f70bb285b64e6b5ee48ff98f2d78
Waryen <jonathangomand@gmail.com>
2022-04-22 15:22:19 +0200

    chore(keyboard): styling

--- apps/solid/src/components/Keyboard.tsx
+++ apps/solid/src/components/Keyboard.tsx
@@ -1,6 +1,10 @@
-import { key } from 'styles/components/key.css';
-import { Setter, onMount, onCleanup, createSignal } from 'solid-js';
+import { Setter, onMount, onCleanup, createSignal, For, Show } from 'solid-js';
 import JSConfetti from 'js-confetti';
+import {
+  styledKeyboard,
+  styledTile,
+  styledRow,
+} from 'styles/components/keyboard.css';
 
 const rows = [
   'qwertyuiop'.split(''),
@@ -12,7 +16,7 @@ type KeyboardProps = {
   setActiveKey: Setter<string>;
 };
 
-export const Keyboard = ({ setActiveKey }: KeyboardProps) => {
+export const Keyboard = (props: KeyboardProps) => {
   onMount(() => {
     document.addEventListener('keydown', keyboardListener);
     document.addEventListener('keyup', cleanKey);
@@ -25,48 +29,50 @@ export const Keyboard = ({ setActiveKey }: KeyboardProps) => {
 
   const keyboardListener = (event: KeyboardEvent) => {
     if (event.key == 'Enter') {
-      setActiveKey(event.key);
+      props.setActiveKey(event.key);
     } else if (event.key == 'Backspace') {
-      setActiveKey(event.key);
+      props.setActiveKey(event.key);
     } else if ('abcdefghijklmnopqrstuvwxyz'.includes(event.key.toLowerCase())) {
-      setActiveKey(event.key);
+      props.setActiveKey(event.key);
     }
   };
 
-  const cleanKey = () => setActiveKey('');
+  const cleanKey = () => props.setActiveKey('');
 
   return (
-    <div>
-      {rows.map((row) => (
-        <div>
-          {row.map((keyValue) => (
-            <button
-              class={key({ color: 'neutral' })}
-              onClick={() => {
-                setActiveKey(keyValue);
-                setActiveKey('');
-              }}
-            >
-              {keyValue === 'Backspace' ? (
-                <svg
-                  v-else
-                  xmlns="http://www.w3.org/2000/svg"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  width="24"
+    <div class={styledKeyboard}>
+      <For each={rows} fallback={<div>Loading...</div>}>
+        {(row) => (
+          <div class={styledRow}>
+            <For each={row} fallback={<div>Loading...</div>}>
+              {(key) => (
+                <button
+                  class={styledTile}
+                  onClick={() => {
+                    props.setActiveKey(key);
+                    props.setActiveKey('');
+                  }}
                 >
-                  <path
-                    fill="currentColor"
-                    d="M22 3H7c-.69 0-1.23.35-1.59.88L0 12l5.41 8.11c.36.53.9.89 1.59.89h15c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H7.07L2.4 12l4.66-7H22v14zm-11.59-2L14 13.41 17.59 17 19 15.59 15.41 12 19 8.41 17.59 7 14 10.59 10.41 7 9 8.41 12.59 12 9 15.59z"
-                  ></path>
-                </svg>
-              ) : (
-                keyValue
+                  <Show when={key === 'Backspace'} fallback={key}>
+                    <svg
+                      v-else
+                      xmlns="http://www.w3.org/2000/svg"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      width="24"
+                    >
+                      <path
+                        fill="currentColor"
+                        d="M22 3H7c-.69 0-1.23.35-1.59.88L0 12l5.41 8.11c.36.53.9.89 1.59.89h15c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H7.07L2.4 12l4.66-7H22v14zm-11.59-2L14 13.41 17.59 17 19 15.59 15.41 12 19 8.41 17.59 7 14 10.59 10.41 7 9 8.41 12.59 12 9 15.59z"
+                      ></path>
+                    </svg>
+                  </Show>
+                </button>
               )}
-            </button>
-          ))}
-        </div>
-      ))}
+            </For>
+          </div>
+        )}
+      </For>
     </div>
   );
 };
@@ -93,20 +99,9 @@ const easterEgg = () => {
     if (pattern.length === current()) {
       setCurrent(0);
 
-      jsConfetti.addConfetti();
-
       jsConfetti.addConfetti({
-        emojis: ['🌈', '❤️', '💥', '✨', '💫', '🌸'],
+        confettiNumber: 1000,
       });
-
-      jsConfetti.addConfetti({
-        confettiRadius: 6,
-        confettiNumber: 2000,
-      });
-
-      jsConfetti
-        .addConfetti()
-        .then(() => console.log('Confetti animation completed!'));
     }
   };
 };
