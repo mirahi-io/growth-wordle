28c24c76c94a9f9a52fe77876d94c34751fcbe8d
Waryen <jonathangomand@gmail.com>
2022-04-25 21:37:11 +0200

    fix(keyboard): keyboard is now responsive

--- libs/styles/components/keyboard.css.ts
+++ libs/styles/components/keyboard.css.ts
@@ -1,4 +1,5 @@
 import { style } from '@vanilla-extract/css';
+import { recipe } from '@vanilla-extract/recipes';
 
 export const styledKeyboard = style({
   display: 'flex',
@@ -7,23 +8,69 @@ export const styledKeyboard = style({
 });
 
 export const styledRow = style({
-  marginBottom: '6px',
   display: 'flex',
 });
 
-export const styledTile = style({
-  fontWeight: 'bold',
-  border: 0,
-  width: '75px',
-  height: '75px',
-  margin: '2px',
-  borderRadius: '4px',
-  cursor: 'pointer',
-  color: '#1a1a1b',
-  flex: 1,
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-  textTransform: 'uppercase',
-  backgroundColor: '#d3d6da',
+export const styledTile = recipe({
+  base: {
+    fontWeight: 'bold',
+    fontSize: '0.5rem',
+    border: 0,
+    margin: '3px',
+    borderRadius: '4px',
+    cursor: 'pointer',
+    color: '#1a1a1b',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    textTransform: 'uppercase',
+    backgroundColor: '#d3d6da',
+    '@media': {
+      'screen and (min-width: 768px)': {
+        fontSize: '0.7rem',
+        margin: '4px',
+      },
+      'screen and (min-width: 1024px)': {
+        fontSize: '1rem',
+        margin: '4px',
+      },
+    },
+  },
+
+  variants: {
+    size: {
+      normal: {
+        width: '30px',
+        height: '30px',
+        '@media': {
+          'screen and (min-width: 768px)': {
+            width: '50px',
+            height: '50px',
+          },
+          'screen and (min-width: 1024px)': {
+            width: '75px',
+            height: '75px',
+          },
+        },
+      },
+      big: {
+        width: '60px',
+        height: '30px',
+        '@media': {
+          'screen and (min-width: 768px)': {
+            width: '100px',
+            height: '50px',
+          },
+          'screen and (min-width: 1024px)': {
+            width: '125px',
+            height: '75px',
+          },
+        },
+      },
+    },
+  },
+
+  defaultVariants: {
+    size: 'normal',
+  },
 });
